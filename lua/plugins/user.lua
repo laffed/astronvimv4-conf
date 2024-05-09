6f3415cc69679a70c5798300114d3b9d6eb5911a
laffed <roarklaffed@gmail.com>
2024-05-08 20:43:23 -0500

    markdown preview

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -68,4 +68,10 @@ return {
       )
     end,
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+  },
 }
