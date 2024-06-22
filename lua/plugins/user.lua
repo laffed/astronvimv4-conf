8b4242cc9f34722fdf289ca8a761d23d8543cbcb
laffed <roarklaffed@gmail.com>
2024-06-21 20:21:28 -0500

    init custom snippets

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -31,15 +31,6 @@ return {
   },
   { "max397574/better-escape.nvim" },
   {
-    "L3MON4D3/LuaSnip",
-    config = function(plugin, opts)
-      require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
-      -- add more custom luasnip configuration such as filetype extend or custom snippets
-      local luasnip = require "luasnip"
-      luasnip.filetype_extend("javascript", { "javascriptreact" })
-    end,
-  },
-  {
     "windwp/nvim-autopairs",
     config = function(plugin, opts)
       require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
