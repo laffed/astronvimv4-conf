c72c7c838aefeea103ae1c76e0855d2079cb8e12
laffed <roarklaffed@gmail.com>
2024-08-20 20:14:44 -0500

    cherry pick manual rust

--- lua/community.lua
+++ lua/community.lua
@@ -5,9 +5,11 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
   { import = "astrocommunity.motion.grapple-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.media.vim-wakatime" },
+  { import = "astrocommunity.programming-language-support.nvim-jqx" },
+  -- LSP Defaults
+  { import = "astrocommunity.pack.lua" },
 }
